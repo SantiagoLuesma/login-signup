51e6655b959600f80adffa033c7d24cf03cbad90
Luesma <seluesma@gmail.com>
2021-09-27 23:05:09 -0300

    Formulario de inicio de sesión creado

---
+++ Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿
+namespace login_signup
+{
+    partial class Formulario
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Formulario));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.login = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.userName = new System.Windows.Forms.TextBox();
+            this.password = new System.Windows.Forms.TextBox();
+            this.exit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(133, 29);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(43, 33);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Poppins", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(87, 83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 37);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Bienvenido";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(39, 154);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(31, 41);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::login_signup.Properties.Resources.key_skeleton;
+            this.pictureBox3.Location = new System.Drawing.Point(39, 226);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(31, 34);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Location = new System.Drawing.Point(39, 194);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(235, 1);
+            this.panel1.TabIndex = 4;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Black;
+            this.panel2.Location = new System.Drawing.Point(39, 259);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(235, 1);
+            this.panel2.TabIndex = 5;
+            // 
+            // login
+            // 
+            this.login.BackColor = System.Drawing.Color.Gold;
+            this.login.FlatAppearance.BorderSize = 0;
+            this.login.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.login.Font = new System.Drawing.Font("Poppins", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.login.Location = new System.Drawing.Point(39, 336);
+            this.login.Name = "login";
+            this.login.Size = new System.Drawing.Size(235, 33);
+            this.login.TabIndex = 6;
+            this.login.Text = "INICIAR SESIÓN";
+            this.login.UseVisualStyleBackColor = false;
+            this.login.Click += new System.EventHandler(this.login_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(49, 397);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(209, 15);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "¿Nuevo en Biostar? Cree una cuenta.";
+            // 
+            // userName
+            // 
+            this.userName.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.userName.Font = new System.Drawing.Font("Poppins", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.userName.Location = new System.Drawing.Point(76, 159);
+            this.userName.Name = "userName";
+            this.userName.PlaceholderText = "Usuario";
+            this.userName.Size = new System.Drawing.Size(198, 29);
+            this.userName.TabIndex = 8;
+            // 
+            // password
+            // 
+            this.password.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.password.Font = new System.Drawing.Font("Poppins", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.password.Location = new System.Drawing.Point(76, 226);
+            this.password.Name = "password";
+            this.password.PasswordChar = '*';
+            this.password.PlaceholderText = "Contraseña";
+            this.password.Size = new System.Drawing.Size(198, 29);
+            this.password.TabIndex = 9;
+            this.password.UseSystemPasswordChar = true;
+            // 
+            // exit
+            // 
+            this.exit.FlatAppearance.BorderSize = 0;
+            this.exit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.exit.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.exit.Location = new System.Drawing.Point(260, 3);
+            this.exit.Name = "exit";
+            this.exit.Size = new System.Drawing.Size(31, 38);
+            this.exit.TabIndex = 11;
+            this.exit.Text = "X";
+            this.exit.UseVisualStyleBackColor = true;
+            this.exit.Click += new System.EventHandler(this.exit_Click);
+            // 
+            // Formulario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(294, 451);
+            this.Controls.Add(this.exit);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.userName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.login);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Formulario";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button login;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox userName;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.Button exit;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace login_signup
+{
+    public partial class Formulario : Form
+    {
+        public Formulario()
+        {
+            InitializeComponent();
+        }
+
+        private void login_Click(object sender, EventArgs e)
+        {
+            string username = userName.Text;
+            string userPassword = password.Text;
+
+            while (username.Trim() == "" || userPassword.Trim() == "")
+            {
+                MessageBox.Show("Ambos campos deben ser completados.");
+                userName.Focus();
+                return;
+            }
+
+            if (username == "demo" && userPassword == "1234")
+            {
+                MessageBox.Show("Ingreso exitoso.");
+                userName.Clear();
+                password.Clear();
+                userName.Focus();
+            }
+            else
+            {
+                MessageBox.Show("El nombre de usuario o la contraseña son incorrectos, inténtelo nuevamente.");
+                userName.Clear();
+                password.Clear();
+                userName.Focus();
+            }
+
+        }
+
+        private void exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
